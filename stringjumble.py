dd500b9550d3958fc15e785a9fb1ab2a41bb5338
DRBarnum <douglasbarnum@hanovernorwichschools.org>
2016-09-26 14:01:08 -0400

    Updated from Brython Server: 9/26/2016 2:01:09 PM

--- stringjumble.py
+++ stringjumble.py
@@ -25,3 +25,10 @@ ydnah dnif yam uoy taht skcirt ro seuqinhcet wef a era erehT
 handy find may you that tricks or techniques few a are There
 erehT era a wef seuqinhcet ro skcirt taht uoy yam dnif ydnah
 """
+import string
+from string import ascii_lowercase 
+words = str(input("Please enter a string of text (the bigger the better): "))
+
+print(words[::-1])
+
+print(words[
